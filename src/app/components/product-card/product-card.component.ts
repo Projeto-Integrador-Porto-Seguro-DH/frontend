96cc79e73d28d162785b4ee984403a482138f78a
KevimLSDS <kevim7@hotmail.com>
2022-06-04 17:07:15 -0300

    Responsividade do card de produto

--- src/app/components/product-card/product-card.component.ts
+++ src/app/components/product-card/product-card.component.ts
@@ -3,13 +3,33 @@ import { Component, OnInit } from '@angular/core';
 @Component({
   selector: 'app-product-card',
   templateUrl: './product-card.component.html',
-  styleUrls: ['./product-card.component.css']
+  styleUrls: ['./product-card.component.css'],
 })
 export class ProductCardComponent implements OnInit {
+  public queijos: any;
 
-  constructor() { }
+  constructor() {}
 
   ngOnInit(): void {
+    this.queijos = [
+      {
+        nome: 'Queijo 1',
+        descricao: 'Queijo muito bom!',
+        preco: 'R$ 300,00',
+        foto: '../../../assets/img/products/queijo/queijo1.jpg',
+      },
+      {
+        nome: 'Queijo 2',
+        descricao: 'Este queijo também é muito bom!',
+        preco: 'R$ 400,00',
+        foto: '../../../assets/img/products/queijo/queijo2.jpg',
+      },
+      {
+        nome: 'Queijo 3',
+        descricao: 'Mais um queijo muito bom!',
+        preco: 'R$ 500,00',
+        foto: '../../../assets/img/products/queijo/queijo3.jpg',
+      },
+    ];
   }
-
 }
--- src/app/components/product-group/product-group.component.ts
+++ src/app/components/product-group/product-group.component.ts
@@ -3,13 +3,10 @@ import { Component, OnInit } from '@angular/core';
 @Component({
   selector: 'app-product-group',
   templateUrl: './product-group.component.html',
-  styleUrls: ['./product-group.component.css']
+  styleUrls: ['./product-group.component.css'],
 })
 export class ProductGroupComponent implements OnInit {
+  constructor() {}
 
-  constructor() { }
-
-  ngOnInit(): void {
-  }
-
+  ngOnInit(): void {}
 }
