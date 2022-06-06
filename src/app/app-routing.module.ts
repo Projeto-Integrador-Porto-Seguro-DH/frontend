cd44d6f64dc022b161f846d386a59c4b6b187a7d
Jessica Santos <jm.jeska@gmail.com>
2022-06-06 17:17:05 -0300

    Alterando rotas e adicionas icon-user na navbar

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,3 +1,4 @@
+import { ModalSearchComponent } from './components/modal-search/modal-search.component';
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
 import { LoginComponent } from './components/login/login.component';
@@ -5,12 +6,13 @@ import { SigninComponent } from './components/signin/signin.component';
 
 import { HomeComponent } from './pages/home/home.component';
 
-const routes: Routes = [{ path: '', component: HomeComponent },
-{path: 'login', component: LoginComponent},
-{path: 'signin', component: SigninComponent}];
+const routes: Routes = [
+  { path: '', component: HomeComponent },
+  { path: 'login', component: LoginComponent },
+  { path: 'signin', component: SigninComponent }];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
   exports: [RouterModule],
 })
-export class AppRoutingModule {}
+export class AppRoutingModule { }
