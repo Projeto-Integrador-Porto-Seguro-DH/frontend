97fdfc75badd790a23d4a7aad5cb3b84ec5e3062
marimontefusco <mari.montefusco@gmail.com>
2022-06-05 21:57:24 -0300

    changing app/pages/user-login-signin-form

--- src/app/pages/user-login-signin-form/user-login-signin-form.component.ts
+++ src/app/pages/user-login-signin-form/user-login-signin-form.component.ts
@@ -1,15 +1,13 @@
-// import { Component, OnInit } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
 
-// @Component({
-//   selector: 'app-user-login-signin-form',
-//   templateUrl: './user-login-signin-form.component.html',
-//   styleUrls: ['./user-login-signin-form.component.css']
-// })
-// export class UserLoginSigninFormComponent implements OnInit {
+@Component({
+  selector: 'app-user-login-signin-form',
+  templateUrl: './user-login-signin-form.component.html',
+  styleUrls: ['./user-login-signin-form.component.css'],
+})
+export class UserLoginSigninFormComponent implements OnInit {
+  constructor() {}
 
-//   constructor() { }
-
-//   ngOnInit(): void {
-//   }
-
-// }
+  ngOnInit(): void {}
+  
+}
