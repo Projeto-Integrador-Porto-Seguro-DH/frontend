9f8bdf70543824f77a30fe1667dbcc6e280950ec
Jessica Santos <jm.jeska@gmail.com>
2022-06-06 16:54:07 -0300

    Adicionando rotas para login e signin

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,9 +1,13 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
+import { LoginComponent } from './components/login/login.component';
+import { SigninComponent } from './components/signin/signin.component';
 
 import { HomeComponent } from './pages/home/home.component';
 
-const routes: Routes = [{ path: '', component: HomeComponent }];
+const routes: Routes = [{ path: '', component: HomeComponent },
+{path: 'login', component: LoginComponent},
+{path: 'signin', component: SigninComponent}];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
