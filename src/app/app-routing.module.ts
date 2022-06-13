bfbd18e39899899595b213c67398f8a02237a7e4
marimontefusco <mari.montefusco@gmail.com>
2022-06-13 13:36:15 -0300

    adicionando rotas: pagina shopping-cart

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,15 +1,16 @@
-import { NgModule } from '@angular/core';
+import { NgModule, Component } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
 
 import { HomeComponent } from './pages/home/home.component';
 import { LoginFormComponent } from './pages/login-form/login-form.component';
+import { ShoppingCartComponent } from './pages/shopping-cart/shopping-cart.component';
 import { SigninFormComponent } from './pages/signin-form/signin-form.component';
 
 const routes: Routes = [
   { path: '', component: HomeComponent },
   { path: 'entrar', component: LoginFormComponent },
-  { path: 'cadastrar', component: SigninFormComponent}
-
+  { path: 'cadastrar', component: SigninFormComponent},
+  { path: 'carrinho', component: ShoppingCartComponent}
 ];
 
 @NgModule({
