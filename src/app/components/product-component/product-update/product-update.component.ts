1b1ee3ef75ca604dea107d1602ac014ae0a121c3
KevimLSDS <kevim7@hotmail.com>
2022-06-27 16:00:57 -0300

    Fixing product CRUD

--- src/app/components/product-component/product-update/product-update.component.ts
+++ src/app/components/product-component/product-update/product-update.component.ts
@@ -9,37 +9,52 @@ import { Categoria } from 'src/app/model/Categoria';
 @Component({
   selector: 'app-product-update',
   templateUrl: './product-update.component.html',
-  styleUrls: ['./product-update.component.css']
+  styleUrls: ['./product-update.component.css'],
 })
 export class ProductUpdateComponent implements OnInit {
-
   constructor(
     private productService: ProductService,
     private route: ActivatedRoute,
-    private categoryService: CategoryService) { }
+    private categoryService: CategoryService
+  ) {}
 
   produto = new Produto();
   categorias: Categoria[];
 
   ngOnInit(): void {
-    this.categoryService.getAllCategories().subscribe((resp: Categoria[])=>{
-      this.categorias = resp;})
+    this.categoryService.getAllCategories().subscribe((resp: Categoria[]) => {
+      this.categorias = resp;
+    });
 
     const id = +this.route.snapshot.paramMap.get('id')!;
-    this.productService.getById(id).subscribe((resp: Produto)=>{
-      this.produto = resp
-    })
+    this.productService.getById(id).subscribe((resp: Produto) => {
+      this.produto = resp;
+      console.log(this.produto);
+      console.log(this.produto.categoria);
+    });
   }
 
   onSubmit() {
-    this.productService.putProduct(this.produto).subscribe((resp: Produto)=>{
+    this.productService.putProduct(this.produto).subscribe((resp: Produto) => {
       this.produto = resp;
       this.productService.showMessage('Produto atualizado com sucesso!');
       this.refresh();
-    })}
-
-  refresh(){
-    window.location.reload()
+    });
   }
 
+  refresh() {
+    window.location.reload();
+  }
+
+  updateCategory(categoriaSelecionada: Categoria) {
+    console.log(categoriaSelecionada);
+    this.produto.categoria = categoriaSelecionada;
+  }
+
+  selected(id: number) {
+    if (this.produto.categoria?.idCategoria == id) {
+      return true;
+    }
+    return null;
+  }
 }
