0e320759252034b1c2f1341f846aeed5caf775f1
Guigaspaco <yoshiro2@gmail.com>
2022-06-28 17:45:33 -0300

    Atualizacoes do Checkout Process

---
+++ src/app/enums/FormaDeEnvio.ts
@@ -0,0 +1,5 @@
+export enum FormaDeEnvio {
+  PAC = 'PAC',
+  SEDEX = 'SEDEX',
+  DIRETO = 'DIRETO',
+}
--- src/app/pages/checkout-process/checkout-process.component.ts
+++ src/app/pages/checkout-process/checkout-process.component.ts
@@ -1,35 +1,41 @@
 import { Component, OnInit } from '@angular/core';
+import { FormaDeEnvio } from 'src/app/enums/FormaDeEnvio';
+import { FormaDePagamento } from 'src/app/enums/PagamentoEnum';
 
 @Component({
   selector: 'app-checkout-process',
   templateUrl: './checkout-process.component.html',
-  styleUrls: ['./checkout-process.component.css']
+  styleUrls: ['./checkout-process.component.css'],
 })
 export class CheckoutProcessComponent implements OnInit {
-  itensComprados:any[];
-  constructor() { }
+  itensComprados: any[];
+  formaDeEnvio = FormaDeEnvio;
+  formaDePagamento = FormaDePagamento;
+  constructor() {}
 
   ngOnInit(): void {
     this.itensComprados = [
-      { nome: 'Queijo Brie',
+      {
+        nome: 'Queijo Brie',
         descricao: 'Inteiro 500gr',
-        preco:'R$ 200,00',
+        preco: 'R$ 200,00',
         categoria: 'Queijos',
         foto: '../../assets/img/products/queijo/queijo4.jpg',
       },
-      { nome: 'Queijo Parmesão',
+      {
+        nome: 'Queijo Parmesão',
         descricao: 'Ralado 150gr',
-        preco:'R$ 18,00',
+        preco: 'R$ 18,00',
         categoria: 'Queijos',
         foto: '../../assets/img/products/queijo/queijo4.jpg',
       },
-      { nome: 'Queijo 3',
+      {
+        nome: 'Queijo 3',
         descricao: 'Ralado 150gr',
-        preco:'R$ 28,00',
+        preco: 'R$ 28,00',
         categoria: 'Queijos',
         foto: '../../assets/img/products/queijo/queijo4.jpg',
       },
     ];
   }
-
 }
